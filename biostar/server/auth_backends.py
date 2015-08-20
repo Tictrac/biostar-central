15130e33be4fa7f177e949d2f7f3322958bbfc57
James Pollock <james@tictrac.com>
2015-08-20 11:45:03 +0100

    Add auth backend

---
+++ biostar/server/auth_backends.py
@@ -0,0 +1,18 @@
+from django_auth_ldap.backend import LDAPBackend
+
+
+class TictracLDAPBackend(LDAPBackend):
+    def get_or_create_user(self, username, ldap_user):
+        model = self.get_user_model()
+        email = ldap_user.attrs.get('mail')
+        if email:
+            username_field = getattr(model, 'USERNAME_FIELD', 'username')
+
+            kwargs = {
+                'email__iexact': email[0],
+                'defaults': {username_field: username.lower()}
+            }
+            user, new = model.objects.get_or_create(**kwargs)
+        else:
+            user, new = super(TictracLDAPBackend, self).get_or_create_user(username, ldap_user)
+        return user, new
